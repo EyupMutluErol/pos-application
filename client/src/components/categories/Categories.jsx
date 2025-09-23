906c2dea605789a21ca7c1cb3c49047a7cd25135
EyupMutluErol <eyupmutluerol@gmail.com>
2025-09-23 13:29:37 +0300

    feat : added Categories and Products components to Home Page

---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <ul className='flex flex-col gap-4 text-2xl'>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Tümü</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Yiyecek</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>İçecek</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Meyve</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Sebze</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Kıyafet</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>Elektronik</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>İçecek</span>
+      </li>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
+        <span>İçecek</span>
+      </li>
+    </ul>
+  )
+}
+
+export default Categories
---
+++ client/src/components/products/Products.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='product-wraper grid grid-cols-6 gap-4'>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
+        <div className="product-img">
+            <img className='h-28 object-cover w-full border-b' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtLo9Vhjatnagk3bNpbj-yg95D91xRn60gxycJmc0EIPSQXRWXIb6rCBiTZYyV4P_isDVUlFzAiTDG5WwMj_9dCOw7aagiLASoMs-HC8aQ"/>
+        </div>
+        <div className="product-info flex flex-col p-3">
+            <span className='font-bold'>Elma</span>
+            <span>12₺</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Products
