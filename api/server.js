d939829e593065df140be2390cc02674212bbedd
EyupMutluErol <eyupmutluerol@gmail.com>
2025-10-26 20:48:40 +0300

    feat : added category , product and bill routes

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 
 const port = 5000;
 
 dotenv.config();
+
+const categoryRoute = require('./routes/categories.js');
+const productRoute = require('./routes/products.js');
+const billRoute = require('./routes/bills.js');
 
 const connect = async () => {
     try {
@@ -16,6 +21,13 @@ const connect = async () => {
         throw error;
     }
 }
+
+app.use(express.json())
+app.use(cors());
+
+app.use('/api/categories',categoryRoute);
+app.use('/api/products',productRoute);
+app.use('/api/bills',billRoute);
 
 app.get('/',(req,res)=>{
     res.send("Hello World!")
